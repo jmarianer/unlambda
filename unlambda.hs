8358f7afabbf096177d25cc6c282ca436df36a7d
Joey Marianer <rmarianer@instructure.com>
2020-08-15 11:44:41 -0700

    Implement .* and r using a writer monad. Implement i. Simple program that prints a star and a newline

--- unlambda.hs
+++ unlambda.hs
@@ -1,28 +1,46 @@
+import Control.Monad.Writer
+
 data Value =
     K
   | K1 Value
   | S
   | S1 Value
   | S2 Value Value
+  | I
+  | PrintChar Char
   deriving Show
 
 data Call =
     CallS
   | CallK
+  | CallI
   | Apply Call Call
+  | CallStar
+  | CallNewline
   deriving Show
 
-eval :: Call -> Value
-eval CallS = S
-eval CallK = K
-eval (Apply f1 f2) = apply (eval f1) (eval f2)
+eval :: Call -> Writer String Value
+eval CallS = return S
+eval CallK = return K
+eval CallI = return I
+eval (Apply f1 f2) = do
+  v1 <- eval f1
+  v2 <- eval f2
+  apply v1 v2
+eval CallStar = return $ PrintChar '*'
+eval CallNewline = return $ PrintChar '\n'
 
-apply :: Value -> Value -> Value
-apply K x = K1 x
-apply (K1 x) y = x
-apply S x = S1 x
-apply (S1 x) y = S2 x y
-apply (S2 x y) z = apply (apply x y) (apply y z)
+apply :: Value -> Value -> Writer String Value
+apply K x             = return $ K1 x
+apply (K1 x) y        = return $ x
+apply S x             = return $ S1 x
+apply (S1 x) y        = return $ S2 x y
+apply (S2 x y) z      = do
+  i1 <- apply x z
+  i2 <- apply y z
+  apply i1 i2
+apply I x             = return x
+apply (PrintChar c) x = writer (x, [c])
 
 main = do
-  print $ eval $ Apply (Apply (Apply CallS CallK) CallS) CallS
+  putStr $ snd $ runWriter $ eval $ Apply CallNewline (Apply CallStar CallK)
